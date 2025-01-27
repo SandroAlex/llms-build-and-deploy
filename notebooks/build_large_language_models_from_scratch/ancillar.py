4000c14e0b5fe104ed1f5701d30968447061d476
Alex Araujo <alex.araujo@br.experian.com>
2025-01-27 14:28:07 +0000

    WIP in chapter 5

--- notebooks/build_large_language_models_from_scratch/ancillar.py
+++ notebooks/build_large_language_models_from_scratch/ancillar.py
@@ -1,7 +1,10 @@
 # Load packages.
 import torch
+import tiktoken
 
 import torch.nn as nn
+
+from torch.utils.data import Dataset, DataLoader
 
 
 ###############################################################################
@@ -247,3 +250,56 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens + 1)
 
     return idx
+
+
+###############################################################################
+# Listing 2.5 A dataset for batched inputs and targets.
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenizes the entire text.
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk  = token_ids[i    : i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+###############################################################################
+# Listing 2.6 A data loader to generate batches with input-with pairs.
+def create_dataloader_v1(
+    txt,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    # drop_last=True:
+    #     drops the last batch if it is shorter than the specified
+    #     batch_size to prevent loss spikes during training.
+    # num_workers:
+    #     The number of CPU processes to use for preprocessing.
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
